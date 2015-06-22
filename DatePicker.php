d20dd10829c22708b8a09b1a990ed77f94fe46d8
joavarvi <VargasJOaquin2014>
2015-06-22 12:42:12 -0600

    Add class DatePicker.php

---
+++ DatePicker.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joaquin
+ * Date: 22/06/15
+ * Time: 12:38 PM
+ */
+
+namespace alkaid\datepicker;
+
+use Yii;
+use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
+use yii\base\InvalidConfigException;
+
+class DatePicker {
+
+}
